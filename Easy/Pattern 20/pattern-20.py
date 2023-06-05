be96c45feb0c42be67a78f1c10bebee9aff17eea
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2023-06-06 02:55:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 20/pattern-20.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        for i in range(1, N+1):
+            print('*'*i, end='')
+            print(' '*((N-i)*2), end='')
+            print('*'*i)
+        for i in range(N-1, 0, -1):
+            print('*'*i, end='')
+            print(' '*((N-i)*2), end='')
+            print('*'*i)
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input().strip())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
