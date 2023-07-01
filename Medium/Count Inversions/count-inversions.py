6b994ce166e115b26988cd3b7cd61741e138fce7
Manobal Singh Bagady <96862518+Manobal-Singh-Bagady@users.noreply.github.com>
2023-07-01 23:43:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count Inversions/count-inversions.py
@@ -0,0 +1,66 @@
+class Solution:
+    count = 0
+    # User function Template for python3
+
+    # arr[]: Input Array
+    # N : Size of the Array arr[]
+    # Function to count inversions in the array.
+    def merge(self, arr, low, mid, high):
+        l = low
+        r = mid + 1
+        temp = []
+        while l <= mid and r <= high:
+            if arr[l] <= arr[r]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                self.count += mid + 1 - l
+                temp.append(arr[r])
+                r += 1
+        while l <= mid:
+            temp.append(arr[l])
+            l += 1
+        while r <= high:
+            temp.append(arr[r])
+            r += 1
+        arr[low : high + 1] = temp[:]
+
+    def mergesort(self, arr, low, high):
+        if low >= high:
+            return
+        mid = (low + high) // 2
+        self.mergesort(arr, low, mid)
+        self.mergesort(arr, mid + 1, high)
+        self.merge(arr, low, mid, high)
+
+    def inversionCount(self, arr, n):
+        self.mergesort(arr, 0, n - 1)
+        return self.count
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for tt in range(t):
+        n = int(input())
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a,n))
+# } Driver Code Ends
